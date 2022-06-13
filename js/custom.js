3033ae7786a11a00d36d49fdc105eb7829cebd0a
Yuri Machado <ymachado1995@gmail.com>
2022-06-13 15:48:56 -0300

    First commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// instancia jquery e evita conflitos
+jQuery(function($) {
+
+    let titulos = $('h4') // tag
+
+    let itens = $('.featured-item') // class
+
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+})
